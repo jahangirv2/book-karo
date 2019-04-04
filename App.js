78b8d617a3734a0942e8e2412d3fdc1c345067a0
jahangirv2 <mimy12345678@hotmail.com>
2019-04-05 00:12:43 +0500

    Login Page Created

--- App.js
+++ App.js
@@ -1,24 +1,81 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Font,AppLoading } from 'expo';
+// import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { Container, Header, Content, Form, Item, Input,Card,CardItem,Body,Text, Title,Row,Col,Button,} from 'native-base';
+import { Login } from './components/views/Login';
+
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
+  async componentWillMount() {
+    await Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+    });
+    this.setState({ loading: false });
+  }
   render() {
+    if(this.state.loading){
+      return(
+        <AppLoading/>
+      );
+    }
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Login/>
+      
+      
+      // <View style={styles.container}>
+      //   <Text style={{fontSize:24,fontWeight:"bold",}}>
+      //     Login
+      //   </Text>
+      //   <TextInput style={styles.inputText} placeholder="Username">
+
+
+      //   </TextInput>
+      // </View>
+      
+    //   <Container>
+    //   <Header />
+    //   <Content padder>
+    //     <Card style={{marginTop:120,alignContent:"center",alignContent:"center",}}>
+    //       <CardItem bordered>
+    //         <Body style={{}}>
+    //           <Text style={{fontSize:24,fontWeight:'bold',alignSelf:"center"}}>
+    //             Login
+    //           </Text>
+    //         </Body>
+    //       </CardItem>
+    //       <CardItem bordered>
+    //       <Body style={{flexDirection:"column",justifyContent:"center"}}>
+    //       <Item rounded>
+    //         <Input placeholder='Username'/>
+    //       </Item>
+    //       <Item style={{marginTop:10}} rounded>
+    //         <Input placeholder='Password'/>
+    //       </Item>
+    //       <Item style={{marginTop:10,alignSelf:"center"}} rounded>
+    //       <Button block rounded style={{width:150}}>
+    //         <Text>
+    //           Sign In!
+    //         </Text>
+    //       </Button>
+    //       </Item>
+          
+    //         <Button style={{alignSelf:"center"}} transparent primary>
+    //         <Text>
+    //           Don't have An Account? Sign Up HERE
+    //         </Text>
+    //         </Button>
+    //       </Body>
+    //       </CardItem>
+    //     </Card>
+    //   </Content>
+    // </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
 
 
---
+++ components/views/Login.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Container, Header, Content, Form, Item, Input,Card,CardItem,Body,Text, Title,Row,Col,Button,} from 'native-base';
+
+export class Login extends React.Component{
+    render(){
+        return(
+            <Container>
+            <Header />
+            <Content contentContainerStyle={{justifyContent:"center",flex:1}} padder>
+              <Card>
+                <CardItem bordered>
+                  <Body style={{}}>
+                    <Text style={{fontSize:24,fontWeight:'bold',alignSelf:"center"}}>
+                      Login
+                    </Text>
+                  </Body>
+                </CardItem>
+                <CardItem bordered>
+                <Body style={{flexDirection:"column",justifyContent:"center"}}>
+                <Item rounded>
+                  <Input placeholder='Username'/>
+                </Item>
+                <Item style={{marginTop:10}} rounded>
+                  <Input placeholder='Password'/>
+                </Item>
+                <Item style={{marginTop:10,alignSelf:"center"}} rounded>
+                <Button block rounded style={{width:150}}>
+                  <Text>
+                    Sign In!
+                  </Text>
+                </Button>
+                </Item>
+                
+                  <Button style={{alignSelf:"center"}} transparent primary>
+                  <Text>
+                    Don't have An Account? Sign Up HERE
+                  </Text>
+                  </Button>
+                </Body>
+                </CardItem>
+              </Card>
+            </Content>
+          </Container>
+            
+        );
+    }
+}
